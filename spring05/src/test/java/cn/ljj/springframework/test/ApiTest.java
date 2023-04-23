c5a944c5c69aa028267a3a12f2225d2a537515a3
LJJ <849346143@qq.com>
2023-04-23 14:44:50 +0800

    feat: 增加classpath、file、url测试

--- spring05/src/test/java/cn/ljj/springframework/test/ApiTest.java
+++ spring05/src/test/java/cn/ljj/springframework/test/ApiTest.java
@@ -1,25 +1,64 @@
 package cn.ljj.springframework.test;
 
+import cn.hutool.core.io.IoUtil;
 import cn.ljj.springframework.beans.PropertyValue;
 import cn.ljj.springframework.beans.PropertyValues;
 import cn.ljj.springframework.beans.factory.config.BeanDefinition;
 import cn.ljj.springframework.beans.factory.config.BeanReference;
 import cn.ljj.springframework.beans.factory.support.DefaultListableBeanFactory;
 import cn.ljj.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import cn.ljj.springframework.core.io.DefaultResourceLoader;
+import cn.ljj.springframework.core.io.Resource;
 import cn.ljj.springframework.test.bean.UserDao;
 import cn.ljj.springframework.test.bean.UserService;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * 简单bean容器
  */
 public class ApiTest {
 
+    private DefaultResourceLoader resourceLoader;
+
+    @Before
+    public void init(){
+        resourceLoader = new DefaultResourceLoader();
+    }
+
+    @Test
+    public void test_classpath() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_file() throws IOException {
+        Resource resource = resourceLoader.getResource("src/test/resources/important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_url() throws IOException {
+        // 网络原因可能导致GitHub不能读取，可以放到自己的Gitee仓库。读取后可以从内容中搜索关键字；OLpj9823dZ
+        Resource resource = resourceLoader.getResource("https://github.com/lyelegant/ljj-spring/blob/main/important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
     /**
      * XML
      */
     @Test
-    public void testBeanFactoryXml(){
+    public void test_xml(){
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 
         XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
